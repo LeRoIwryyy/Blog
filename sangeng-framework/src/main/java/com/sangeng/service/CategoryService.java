a033befa4ed4261e3edf9982a4984c5653a15542
LeRoIwryyy <LeRoI233NieR@gmail.com>
2024-11-20 10:56:17 +0800

    first commit

---
+++ sangeng-framework/src/main/java/com/sangeng/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.sangeng.service;
+
+import com.sangeng.domain.ResponseResult;
+
+public interface CategoryService {
+    //查询文章分类的接口
+    ResponseResult getCategoryList();
+}
---
+++ sangeng-framework/src/main/java/com/sangeng/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,61 @@
+package com.sangeng.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sangeng.constants.SystemCanstants;
+import com.sangeng.domain.ResponseResult;
+import com.sangeng.domain.entity.Article;
+import com.sangeng.domain.entity.Category;
+import com.sangeng.mapper.CategoryMapper;
+import com.sangeng.service.ArticleService;
+import com.sangeng.service.CategoryService;
+import com.sangeng.utils.BeanCopyUtils;
+import com.sangeng.vo.CategoryVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service("categoryService")
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+
+    //ArticleService是我们在huanf-framework写的接口
+    @Autowired
+    private ArticleService articleService;
+
+    //查询分类列表的核心代码
+    @Override
+    public ResponseResult getCategoryList(){
+        LambdaQueryWrapper<Article> articleWrapper = new LambdaQueryWrapper<>();
+        //要求查的是getStatus字段的值为1，注意SystemCanstants是我们写的一个常量类，用来解决字面值的书写问题
+        articleWrapper.eq(Article::getStatus, SystemCanstants.ARTICLE_STATUS_NORMAL);
+        //查询文章列表，条件就是上一行的articleWrapper
+        List<Article> articlelist = articleService.list(articleWrapper);
+        //获取文章的分类id，并且去重。使用stream流来处理上一行得到的文章表集合
+        Set<Long> categoryIds = articlelist.stream()
+                //下面那行可以优化为Lambda表达式+方法引用
+                .map(article -> article.getCategoryId())
+                //把文章的分类id转换成Set集合
+//              .map(new Function<Article, Long>() {
+//                    @Override
+//                    public Long apply(Article article) {
+//                        return article.getCategoryId();
+//                    }
+//                })
+                .collect(Collectors.toSet());
+        //查询分类表
+        List<Category> categories  = listByIds(categoryIds);
+        //注意SystemCanstants是我们写的一个常量类，用来解决字面值的书写问题
+        categories = categories.stream()
+                .filter(category -> SystemCanstants.STATUS_NORMAL.equals(category.getStatus()))
+                .collect(Collectors.toList());
+
+        //封装成CategoryVo实体类后返回给前端，CategoryVo的作用是只返回前端需要的字段。BeanCopyUtils是我们写的工具类
+        List<CategoryVO> categoryVOS = BeanCopyUtils.copyBeanList(categories, CategoryVO.class);
+        return ResponseResult.okResult(categoryVOS);
+    }
+}
